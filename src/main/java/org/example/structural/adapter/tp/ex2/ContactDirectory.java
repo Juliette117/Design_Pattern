6139fd85149417c0239b24943d0a38ae80e8ae97
Juliette <juliette.ancla@protonmail.com>
2025-12-12 14:16:13 +0100

    tp adapter

--- src/main/java/org/example/structural/adapter/tp/ex2/ContactDirectory.java
+++ src/main/java/org/example/structural/adapter/tp/ex2/ContactDirectory.java
@@ -1,16 +1,19 @@
 package org.example.structural.adapter.tp.ex2;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ContactDirectory {
-    private Map<String, string> map = new HashMap<>();
+    private Map<String, String> map = new HashMap<>();
 
     public ContactDirectory() {
         map.put("0300000001", "a@a.com" );
         map.put("0300000002", "b@a.com");
     }
 
-    public String getEmail(String )
+    public String getEmail(String phoneNumber ) {
+        return map.get(phoneNumber);
+    }
 
 
 }
--- src/main/java/org/example/structural/adapter/tp/ex2/SmsService.java
+++ src/main/java/org/example/structural/adapter/tp/ex2/SmsService.java
@@ -2,5 +2,5 @@ package org.example.structural.adapter.tp.ex2;
 
 public interface SmsService {
 
-    void sendSms(String number, String message);
+    void sendSms(String phoneNumber, String message);
 }
