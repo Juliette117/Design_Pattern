4f15a9d850b1c5403ab3b0867c7c7b81c16a40d9
Juliette <juliette.ancla@protonmail.com>
2025-12-12 14:22:43 +0100

    tp adapter

--- src/main/java/org/example/structural/adapter/tp/ex2/NotificationAdapter.java
+++ src/main/java/org/example/structural/adapter/tp/ex2/NotificationAdapter.java
@@ -3,12 +3,15 @@ package org.example.structural.adapter.tp.ex2;
 public class NotificationAdapter implements SmsService {
 
     private EmailService emailService;
+    private ContactDirectory directory;
 
-    public NotificationAdapter(EmailService emailService) {
+    public NotificationAdapter(EmailService emailService, ContactDirectory directory) {
         this.emailService = emailService;
+        this.directory = directory;
     }
     @Override
-    public void sendSms(String number, String message) {
+    public void sendSms(String phoneNumber, String message) {
+        String email = directory.getEmail(phoneNumber);
 
     }
 }
